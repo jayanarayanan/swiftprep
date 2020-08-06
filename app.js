48da6074b025a09a82722c77ebb4ba6431ae4bf0
Jayanarayanan <jay.jvpd@gmail.com>
2020-08-06 16:16:06 +0530

    first commit

--- app.js
+++ app.js
@@ -1,76 +1,123 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
-const multer = require("multer");
-const aws = require('aws-sdk');
-const multers3 = require('multer-s3');
-const { Server } = require("http");
-
-// const storage = multer.diskStorage({
-//     destination: './public/uploads',
-//     filename: function(req, file, cb) {
-//         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-//     }
-// })
-
-const s3 = new aws.S3({
-    accessKeyId: 'AKIAJU7CNRSTYYSY5L6Q',
-    secretAccessKey: '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',
-    Bucket: 'startupbucket'
-})
-
-const upload = multer({
-    storage: multers3({
-        s3: s3,
-        bucket: 'startupbucket',
-        acl: 'public-read',
-        contentLength: '5000000000',
-        filename: function(req, file, cb) {
-            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); 
-        }
-    }),
-    fileFilter: function(req, file, cb) {
-        checkFileType(file, cb);
-    }
-}).single('file');
-
-function checkFileType(file, cb) {
-    const fileTypes = /mov|mp4|m4v|wmv|mkv|jpg|jpeg/;
-    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = fileTypes.test(file.mimetype);
-    if(mimetype && extname) {
-        return cb(null, true);
-    } else {
-        cb('Error: Video only');
-    }
-}
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const passport = require('passport');
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require("./rootaccess.js");
+const middleware = require("./middleware");
+const { PassThrough } = require("stream");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/swiftprep-videos", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
 app.set('view engine', 'ejs');
 
-app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(cookieSession({
+    maxAge: 6*60*60*1000,
+    keys: [keys.session.cookieKey]
+}))
+
+//MongoDB Schemas
+var videoSchema = new mongoose.Schema({
+    VName: String,
+    Mentor: String,
+});
+var Video = mongoose.model("Video", videoSchema);
+
+var userSchema = new mongoose.Schema({
+    username: String,
+    googleID: String,
+});
+var User = mongoose.model("User", userSchema);
+
+//Passport config
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser((user, done)=> {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    })
+})
+
+passport.use(
+    new GoogleStrategy({
+        // options for google strategy
+        callbackURL: '/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        User.findOne({googleID: profile.id}).then((currentUser) => {
+            if(currentUser) {
+                console.log("user is : " + currentUser);
+                done(null, currentUser);
+            } else {
+                new User({
+                    username: profile.displayName,
+                    googleID: profile.id,
+                }).save().then((newUser) => {
+                    console.log("new user created: " + newUser);
+                    done(null, newUser);
+                });
+            }
+        })
+    })
+);
 
 
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+ });
+
+ //homepage
 app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.post('/upload', function(req, res) {
-    upload(req, res, function(err) {
-        if(err) {
-            res.render('index', {msg: err});
-        } else {
-            if(req.file == undefined) {
-                res.render('index', {msg: 'No file selected.'});
-            } else {
-                res.render('index', {msg: 'File uploaded.'});
-                console.log(req.file);
-            }
-        }
-    })
-})
-
-app.listen(3000, "localhost", function(){
-   console.log("SERVER IS RUNNING!");
+//Search results
+app.post('/search', function(req, res) {
+    var s = req.body.search;
+    res.render('results', {link: keys.aws.urlCode, file: s});
 });
+
+//View private page(only if the user is logged in)
+app.get('/view', function(req, res) {
+    if(req.user) {
+        res.render('results', {link: keys.aws.urlCode, file: 'BITS-CS-5-MI-2'});
+    } else {
+        res.redirect('/google');
+    }
+    
+});
+
+//Login page
+app.get('/google', passport.authenticate('google', {
+    scope: ['profile'],
+})
+);
+
+//Passport auth
+app.get('/google/redirect', passport.authenticate('google'), function(req, res) {
+    res.redirect('/');
+});
+
+//logout page
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+//listener
+app.listen(3000, "localhost", function(){
+    console.log("SERVER IS RUNNING!");
+ });
---
+++ rootaccess.js
@@ -0,0 +1,18 @@
+//Add this file to .gitignore
+
+module.exports = {
+    aws: {
+        clientID: 'AKIAWNC4MSZPEZPDQZ4F',
+        clientSecret: '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',
+        bucket: 'swiftprep-videos',
+        region: 'ap-south-1',
+        urlCode: 'd1cxww848o7ex7',
+    },
+    google: {
+        clientID: "813942541753-ibok0740c59fi8m3umoe4jcnj73d8e9v.apps.googleusercontent.com",
+        clientSecret: "iFkoHL15SIba8_QJ1xmdZ7m1"
+    },
+    session: {
+        cookieKey: "ValarMorghulis",
+    }
+}
