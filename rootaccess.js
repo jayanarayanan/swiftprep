9df7ecf1b7d4ee5d0221c7b639e426af2c0afc3e
jayanarayanan <jay.jvpd@gmail.com>
2020-09-02 13:19:28 +0530

    heroku oauth fix

--- rootaccess.js
+++ rootaccess.js
@@ -9,10 +9,8 @@ module.exports = {
         urlCode: 'd1cxww848o7ex7',
     },
     google: {
-        // clientID: "813942541753-ibok0740c59fi8m3umoe4jcnj73d8e9v.apps.googleusercontent.com",
-        clientID: "eb39e881-a20a-449e-9add-cdabdedd55c2",
-        // clientSecret: "iFkoHL15SIba8_QJ1xmdZ7m1"
-        clientSecret: "f2e2c71d-900d-4ec4-a53a-6f24433c0db4"
+        clientID: "813942541753-ibok0740c59fi8m3umoe4jcnj73d8e9v.apps.googleusercontent.com",
+        clientSecret: "iFkoHL15SIba8_QJ1xmdZ7m1"
     },
     session: {
         cookieKey: "ValarMorghulis",
